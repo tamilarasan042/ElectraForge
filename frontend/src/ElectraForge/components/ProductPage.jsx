d439fab864ab578b11c0eb3ddf4d5cdc92cc39a1
Tamilarasan64 <tamilarasans042@gmail.com>
2026-01-02 11:21:05 +0530

    Fix BASE_URL undefined for images

--- frontend/src/ElectraForge/components/ProductPage.jsx
+++ frontend/src/ElectraForge/components/ProductPage.jsx
@@ -56,17 +56,17 @@ const [activeVideoId, setActiveVideoId] = useState(null);
         {electronics.map((item) => (
           <div key={item._id} style={styles.card}>
             <img
-              src={
-                item.image
-                  ? item.image.startsWith("http")
-                    ? item.image
-                    : `http://localhost:5000/uploads/${item.image}`
-                  : "https://via.placeholder.com/180"
-              }
-              alt={item.name}
-              style={styles.image}
-            />
-            
+  src={
+    item.image
+      ? item.image.startsWith("http")
+        ? item.image
+        : `${import.meta.env.VITE_API_URL.replace("/api", "")}/uploads/${item.image}`
+      : "https://via.placeholder.com/180"
+  }
+  alt={item.name}
+  style={styles.image}
+/>
+
 
             <h3 style={styles.name}>{item.name}</h3>
             <p style={styles.desc}>{item.description}</p>
